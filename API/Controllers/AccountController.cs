c4fd6df944e8e3d15641d007d8f15a3f9bb1b344
Salah Haciakil <salahaljable@gmail.com>
2021-07-01 18:16:20 +0300

    git

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+        private ITokenService tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            this.context = context;
+            this.tokenService = tokenService;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto userInfo)
+        {
+            if (await this.isUserNameExist(userInfo.UserName.ToLower())) { return BadRequest("User is taken"); }
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser()
+            {
+                UserName = userInfo.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(userInfo.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            this.context.Users.Add(user);
+            await this.context.SaveChangesAsync();
+            return new UserDto()
+            {
+                UserName = user.UserName,
+                Token = this.tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> isUserNameExist(string userName)
+        {
+            return await this.context.Users.AnyAsync(x => x.UserName == userName);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto userInfo)
+        {
+            var user = await this.context.Users.SingleOrDefaultAsync(x =>
+            x.UserName.ToLower() == userInfo.UserName.ToLower());
+
+
+            if (user == null) { return Unauthorized("Invalid userName"); }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            byte[] userInfoPasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(userInfo.Password));
+
+            if (!userInfoPasswordHash.SequenceEqual(user.PasswordHash)) return Unauthorized("Invalid password");
+
+            return new UserDto()
+            {
+                UserName = user.UserName,
+                Token = this.tokenService.CreateToken(user)
+            };
+        }
+
+
+
+
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -14,8 +14,12 @@ using Microsoft.OpenApi.Models;
 
 using API.Data;
 using Microsoft.EntityFrameworkCore;
-
-
+using API.Services;
+using API.interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using API.Extensions;
 
 namespace API
 {
@@ -32,14 +36,13 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddApplicationServices(this.config);
             services.AddControllers();
             services.AddCors();
-            services.AddDbContext<DataContext>(options =>
+            services.AddApplicationIdentityServices(this.config);
 
-            {
-                options.UseSqlite(this.config.GetConnectionString("DefaultConnection"));
-            });
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -61,6 +64,8 @@ namespace API
             app.UseRouting();
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
